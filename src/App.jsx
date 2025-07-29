95fd6c27e5e04ab1bfae66d2af0c1f1282dd63b7
Nick Kim <nickhkim17@gmail.com>
2025-07-29 13:25:56 +0900

    Change h1 title to input with placeholder allowing for users to enter their own title more intuitively

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 const days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-const hours = Array.from({ length: 24 }, (_, i) => `${i}:00`);
+const hours = Array.from({ length: 16 }, (_, i) => `${i+8}:00`);
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center p-6">
-      <h1 className="text-5xl font-bold mb-6">Linked Up</h1>
-      
+    <div className="min-h-screen bg-gray-100 p-6 pl-50">
+      <input
+        type="text"
+        placeholder="Enter title..."
+        className="text-4xl mb-4 border-none ml-22"
+        onKeyDown={(e) => { //Event handler for pressing enter
+          if (e.key === "Enter") {
+            e.preventDefault();      // prevents form submission or line breaks
+            e.currentTarget.blur();  // removes focus (cursor disappears)
+          }
+        }}
+      />
+
       <div className="flex">
         {/* Hour labels */}
         <div className="flex flex-col mr-2">
-          <div className="h-10"></div>
+          <div className="h-8.5"></div>
           {hours.map((hour) => (
-            <div key={hour} className="h-10 w-12 text-xs text-right pr-2">{hour}</div>
+            <div key={hour} className="h-10 w-20 text-xs text-right pr-3">{hour}</div>
           ))}
         </div>
 
@@ -29,7 +39,7 @@ const App = () => {
             days.map((day) => (
               <div
                 key={`${day}-${hour}`}
-                className="h-10 w-16 border border-gray-300 hover:bg-green-300 transition-colors"
+                className="h-10 w-20 border border-gray-300 hover:bg-green-300 transition-colors"
               ></div>
             ))
           )}
